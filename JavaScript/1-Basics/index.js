925b5047066a634b9192a24089c5ae2d41113050
Yunus Emre İnci <yeinci54@gmail.com>
2026-07-07 16:46:31 +0300

    learned JS basics

---
+++ JavaScript/1-Basics/index.js
@@ -0,0 +1,20 @@
+let firstBatch = 5
+let secondBatch = 7
+
+let count = firstBatch + secondBatch
+
+document.getElementById("count-of-people").innerText = count
+
+let myAge = 21
+let humanDogRatio = 7
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge)
+
+let bonusPoints = 50
+
+bonusPoints += 50
+bonusPoints /= 4
+bonusPoints += 45
+
+console.log(bonusPoints)
